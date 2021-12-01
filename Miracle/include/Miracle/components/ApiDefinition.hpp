1da8e9b761c576292523c43b292dc47a05d17e12
McFlyboy <hsn95@live.com>
2021-12-01 18:19:52 +0100

    Changed some variable names

--- Miracle/include/Miracle/components/ApiDefinition.hpp
+++ Miracle/include/Miracle/components/ApiDefinition.hpp
@@ -1,6 +1,6 @@
 #pragma once
 
-#if defined(MIRACLE_PLATFORM_WINDOWS) && defined(MIRACLE_LINKAGE_SHARED)
+#if defined(MIRACLE_PLATFORM_WINDOWS) && defined(MIRACLE_LINKAGE_DYNAMIC)
 #if defined(MIRACLE_USAGE_BUILD)
 #define MIRACLE_API __declspec(dllexport)
 #else
