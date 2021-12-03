684620cc592756fb2c04fb963ff2318b0bbda319
McFlyboy <hsn95@live.com>
2021-12-03 01:20:31 +0100

    Removed test stuff

--- Miracle/src/Miracle/MiracleApp.cpp
+++ Miracle/src/Miracle/MiracleApp.cpp
@@ -2,11 +2,9 @@
 
 #include <iostream>
 
-#include <GLFW/glfw3.h>
-
 namespace Miracle {
 	int MiracleApp::run() {
-		std::cout << glfwGetVersionString() << std::endl;
+		std::cout << "Hello Miracle!" << std::endl;
 
 		return 0;
 	}
