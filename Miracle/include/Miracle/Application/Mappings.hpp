977440a684b19ef4b3d6501ef810503657374838
McFlyboy <hsn95@live.com>
2023-02-20 19:57:17 +0100

    Added some vector math and changing a few things

---
+++ Miracle/include/Miracle/Application/Mappings.hpp
@@ -0,0 +1,50 @@
+#pragma once
+
+#include <string_view>
+#include <utility>
+
+#include <Miracle/Common/Models/WindowConfig.hpp>
+#include <Miracle/Common/Models/RendererConfig.hpp>
+#include <Miracle/Common/Models/SceneConfig.hpp>
+#include "Graphics/Renderer.hpp"
+#include "Models/Scene.hpp"
+#include "IWindow.hpp"
+
+namespace Miracle::Application {
+	class Mappings {
+	public:
+		Mappings() = delete;
+
+		static inline WindowInitProps toWindowInitProps(
+			const WindowConfig& windowConfig,
+			const std::u8string_view& defaultTitle
+		) {
+			return WindowInitProps{
+				.title     = windowConfig.title.has_value() ? windowConfig.title.value() : defaultTitle,
+				.size      = windowConfig.size,
+				.resizable = windowConfig.resizable
+			};
+		}
+
+		static inline RendererInitProps toRendererInitProps(
+			const RendererConfig& rendererConfig
+		) {
+			return RendererInitProps{
+				.swapchainInitProps = SwapchainInitProps{
+					.useSrgb  = false,
+					.useVsync = false
+				},
+				.mesh               = rendererConfig.mesh
+			};
+		}
+
+		static inline SceneInitProps toSceneInitProps(
+			const SceneConfig& sceneConfig
+		) {
+			return SceneInitProps{
+				.backgroundColor = sceneConfig.backgroundColor,
+				.entityConfigs   = sceneConfig.entityConfigs
+			};
+		}
+	};
+}
